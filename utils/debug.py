b801f35628dfe873971348b37b57bd15b2603d9e
Eli Chan <eli.chan@intertwine.io>
2018-04-18 18:59:39 -0500

    Make cosmetic improvements to async_debug messages

--- utils/debug.py
+++ utils/debug.py
@@ -35,17 +35,15 @@ def async_debug(offset=None, indent=4):
             frame_records = inspect.stack()
             offset = sum(1 for f in frame_records
                          if f.function == 'async_debug_wrapper') - 1
-
         offset_space = ' ' * offset * indent
         pp = PrettyPrinter(indent=indent, width=WIDTH)
+
         print(SEPARATOR)
         print(f'{offset_space}Awaiting {wrapped.__name__}')
         if instance is not None:
             format_text('instance', repr(instance), offset_space)
-
         format_text('args', pp.pformat(args), offset_space)
         format_text('kwargs', pp.pformat(kwargs), offset_space)
-
         loop = asyncio.get_event_loop()
         start_time = loop.time()
         format_text('start', str(start_time), offset_space)
@@ -60,13 +58,11 @@ def async_debug(offset=None, indent=4):
         print(f'{offset_space}Result returned from {wrapped.__name__}')
         if instance is not None:
             format_text('instance', repr(instance), offset_space)
-
-        format_text('end', str(end_time), offset_space)
-        format_text('elapsed', str(elapsed), offset_space)
-
         format_text('args', pp.pformat(args), offset_space)
         format_text('kwargs', pp.pformat(kwargs), offset_space)
-        format_text('result', pp.pformat(result), offset_space)
+        format_text('return', pp.pformat(result), offset_space)
+        format_text('end', str(end_time), offset_space)
+        format_text('elapsed', str(elapsed_time), offset_space)
         print(SEPARATOR)
 
         return result
