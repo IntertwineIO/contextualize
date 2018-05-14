6eb61388fa218959d10f7f283387ad57f6e9888c
Eli Chan <eli.chan@intertwine.io>
2018-05-14 02:02:05 -0500

    Rename Datetimes to align with FlexTime changes

--- utils/time.py
+++ utils/time.py
@@ -7,39 +7,39 @@ from utils.structures import FlexEnum
 
 TZINFO_TAG = 'tzinfo'
 
-DatetimeInfo = namedtuple(
-    'DatetimeInfo',
+DateTimeInfo = namedtuple(
+    'DateTimeInfo',
     f'year month day hour minute second microsecond {TZINFO_TAG} fold')
 
-TZINFO_IDX = DatetimeInfo._fields.index(TZINFO_TAG)
+TZINFO_IDX = DateTimeInfo._fields.index(TZINFO_TAG)
 
 Granularity = FlexEnum('Granularity',
-                       [f.upper() for f in DatetimeInfo._fields[:TZINFO_IDX]])
+                       [f.upper() for f in DateTimeInfo._fields[:TZINFO_IDX]])
 
 
-class Datetime(datetime):
+class DateTimeWrapper(datetime):
     """
-    Datetime is a light-weight datetime subclass that allows custom
-    attributes to be set on it.
+    DateTimeWrapper is a light-weight datetime subclass that allows
+    custom attributes to be set on it.
     """
     @classmethod
     def from_datetime(cls, dt):
-        kwds = {f: getattr(dt, f) for f in DatetimeInfo._fields}
+        kwds = {f: getattr(dt, f) for f in DateTimeInfo._fields}
         return cls(**kwds)
 
     def to_datetime(self):
-        kwds = {f: getattr(self, f) for f in DatetimeInfo._fields}
+        kwds = {f: getattr(self, f) for f in DateTimeInfo._fields}
         return datetime(**kwds)
 
 
 def flex_strptime(string, formats):
     """
     Flex strptime attempts datetime.strptime with each of the given
-    formats until successful and returns a Datetime with the
+    formats until successful and returns a DateTimeWrapper with the
     corresponding granularity.
 
     I/O:
-    string:     datetime string to be converted to a Datetime
+    string:     datetime string to be converted to a DateTimeWrapper
     formats:    sequence of formats, where each format supports
                 the following strptime directives:
                 %Y/%y   Year with/without century as zero-padded number
@@ -49,7 +49,7 @@ def flex_strptime(string, formats):
                 %M      Minute as a zero-padded decimal number
                 %S      Second as a zero-padded decimal number
                 %f      Microsecond as a zero-padded decimal number
-    return:     Datetime instance with a granularity attribute
+    return:     DateTimeWrapper instance with a granularity attribute
     """
     num_templates = len(formats)
     for i, template in enumerate(formats, start=1):
@@ -60,7 +60,7 @@ def flex_strptime(string, formats):
                 raise ValueError(f"time data '{string}' does not "
                                  f"match any formats: {formats}")
         else:
-            dt_wrapper = Datetime.from_datetime(dt)
+            dt_wrapper = DateTimeWrapper.from_datetime(dt)
             gval = template.count('%')
             dt_wrapper.granularity = Granularity(gval)
             return dt_wrapper
