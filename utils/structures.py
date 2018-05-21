a4e338ff2341926489521a9af944669678bbb7a0
Eli Chan <eli.chan@intertwine.io>
2018-05-21 18:11:00 -0500

    Add CacheKey for composing keys from fields/qualifiers

--- utils/structures.py
+++ utils/structures.py
@@ -5,6 +5,85 @@ from enum import Enum
 from functools import lru_cache
 
 
+class CacheKey:
+    """
+    CacheKey
+
+    A class for composing cache keys from fields and/or qualifiers.
+
+    Control characters 0 and 31 are used as delimiters to minimize
+    conflicts, while '=' and '&' are substituted for display purposes.
+
+    I/O:
+    fields=None      Ordered dictionary of name/value string pairs
+    qualifiers=None  Sequence of strings
+    """
+    NAME_VALUE_DELIMITER = chr(0)
+    NAME_VALUE_DELIMITER_DISPLAY = '='
+
+    TERM_DELIMITER = chr(31)
+    TERM_DELIMITER_DISPLAY = '&'
+
+    @classmethod
+    def from_key(cls, key, is_display=None):
+        """From Key constructs a CacheKey instance from a key string"""
+        if is_display is None:
+            is_display = (cls.TERM_DELIMITER not in key and
+                          cls.NAME_VALUE_DELIMITER not in key)
+
+        term_delimiter, name_value_delimiter = (
+            (cls.TERM_DELIMITER_DISPLAY, cls.NAME_VALUE_DELIMITER_DISPLAY)
+            if is_display else (cls.TERM_DELIMITER, cls.NAME_VALUE_DELIMITER))
+
+        terms = key.split(term_delimiter)
+        i = len(terms)
+        while i and name_value_delimiter not in terms[i - 1]:
+            i -= 1
+
+        try:
+            fields = OrderedDict(term.split(name_value_delimiter) for term in terms[:i])
+        except ValueError:
+            raise ValueError('CacheKey fields must precede all qualifiers')
+
+        qualifiers = terms[i:]
+        return cls(fields, qualifiers)
+
+    def _form_key(self, is_display=False):
+        term_delimiter, name_value_delimiter = (
+            (self.TERM_DELIMITER_DISPLAY, self.NAME_VALUE_DELIMITER_DISPLAY)
+            if is_display else (self.TERM_DELIMITER, self.NAME_VALUE_DELIMITER))
+
+        terms = []
+        for field, value in self.fields.items():
+            terms.append(f'{field}{name_value_delimiter}{value}')
+
+        if self.qualifiers:
+            terms.extend(self.qualifiers)
+        return term_delimiter.join(terms)
+
+    def to_key(self, is_display=False):
+        """To Key forms a key string from a CacheKey instance"""
+        return self._form_key(is_display)
+
+    @property
+    def key(self):
+        """Key property forms a key string from a CacheKey instance"""
+        return self._form_key(is_display=False)
+
+    def __repr__(self):
+        return self._form_key(is_display=True)
+
+    def __init__(self, fields=None, qualifiers=None):
+        self.fields = fields
+        self.qualifiers = qualifiers
+
+    def __eq__(self, other):
+        return self.fields == other.fields and self.qualifiers == other.qualifiers
+
+    def __ne__(self, other):
+        return self.fields != other.fields or self.qualifiers != other.qualifiers
+
+
 class FlexEnum(Enum):
     """
     FlexEnum, the flexible Enum
