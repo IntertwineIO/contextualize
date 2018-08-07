0fc08303e000ce2d22f1458d5370402618afb6dc
Eli Chan <eli.chan@intertwine.io>
2018-08-06 19:02:13 -0500

    Add serialization utility tests

---
+++ tests/utils/test_serialization.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import pytest
+from datetime import datetime, date, time
+from decimal import Decimal
+from enum import Enum
+
+from extractor import BaseExtractor
+from utils.enum import FlexEnum
+from utils.serialization import NULL, serialize, serialize_nonstandard
+
+
+class Color:
+
+    AdditivePrimary = FlexEnum('AdditivePrimary', 'RED GREEN BLUE')
+
+    class SubtractivePrimary(Enum):
+        RED = 1
+        YELLOW = 2
+        BLUE = 3
+
+    mix_types = {AdditivePrimary, SubtractivePrimary}
+
+    def to_json(self):
+        return f"Color(Color.{self.mix_type.__name__}, '{self.color.name}')"
+
+    def __init__(self, mix_type, color):
+        assert mix_type in self.mix_types
+        self.mix_type = mix_type
+        self.color = self.mix_type[color]
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    ('value', 'check'), [
+    (datetime(2018, 7, 8, 5, 43, 21, 12345), '2018-07-08T05:43:21.012345'),
+    (date(1918, 7, 14), '1918-07-14'),
+    (time(12, 34, 56, 7890), '12:34:56.007890'),
+    (Decimal('2.718281828459'), '2.718281828459'),
+    (Color.AdditivePrimary.GREEN, 'test_serialization.Color.AdditivePrimary.GREEN'),
+    (Color.SubtractivePrimary.YELLOW, 'test_serialization.Color.SubtractivePrimary.YELLOW'),
+    (BaseExtractor.WebDriverType.CHROME, 'extractor.BaseExtractor.WebDriverType.CHROME'),
+])
+def test_serialize_nonstandard(value, check):
+    """Test serialize "nonstandard" (i.e. not handled by json.dumps)"""
+    serialized = serialize_nonstandard(value)
+    assert serialized == check
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    ('value', 'check'), [
+    (None, NULL),
+    (Color(Color.AdditivePrimary, 'GREEN'), "Color(Color.AdditivePrimary, 'GREEN')"),
+    (Color(Color.SubtractivePrimary, 'YELLOW'), "Color(Color.SubtractivePrimary, 'YELLOW')"),
+    (Color.AdditivePrimary.GREEN, 'test_serialization.Color.AdditivePrimary.GREEN'),
+    (Color.SubtractivePrimary.YELLOW, 'test_serialization.Color.SubtractivePrimary.YELLOW'),
+    (Color.AdditivePrimary, str(Color.AdditivePrimary))
+])
+def test_serialize(value, check):
+    serialized = serialize(value)
+    assert serialized == check
