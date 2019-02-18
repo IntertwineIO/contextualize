25d18ac12412758c1ed0ad965c554cab37c43236
Eli Chan <eli.chan@intertwine.io>
2019-02-18 08:48:09 -0600

    Move reset_file to utils.testing.tools

--- contextualize/utils/tools.py
+++ contextualize/utils/tools.py
@@ -374,33 +374,6 @@ def numify(text, default=object):
         return default
 
 
-def read_file(path):
-    """Read file at given path"""
-    with open(path) as file:
-        return file.read()
-
-
-def write_file(path, content):
-    """Write file at given path with given content"""
-    with open(path, 'w') as file:
-        file.write(content)
-
-
-@contextmanager
-def reset_files(*paths):
-    """Context manager that resets contents of specified file paths"""
-    initial_content = {path: None for path in paths}
-    for path in initial_content.keys():
-        initial_content[path] = read_file(path)
-
-    try:
-        yield initial_content.items()
-
-    finally:
-        for path, content in initial_content.items():
-            write_file(path, content)
-
-
 def logical_xor(a, b):
     """Logical xor of a and b, returning bool"""
     return bool(a) ^ bool(b)
