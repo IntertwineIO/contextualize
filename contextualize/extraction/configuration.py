079bff87bd669665beab7a63796496d43e3f50af
Eli Chan <eli.chan@intertwine.io>
2019-03-10 00:44:42 -0600

    Eliminate ItemsConfiguration and other configuration refactors

--- contextualize/extraction/configuration.py
+++ contextualize/extraction/configuration.py
@@ -14,7 +14,24 @@ from contextualize.utils.tools import PP, xor_constrain
 
 
 class BaseConfiguration(DotNotatableOrderedDict):
-    pass
+
+    @classmethod
+    def configure_field(cls, configuration, field, source, extractor):
+        # TODO: utilize contextvars for field/source (py3.7)
+        if isinstance(configuration, dict):
+            return ExtractionOperation.from_dict(configuration=configuration,
+                                                 field=field,
+                                                 source=source,
+                                                 extractor=extractor)
+
+        if isinstance(configuration, list):
+            return [ExtractionOperation.from_dict(configuration=operation_config,
+                                                  field=field,
+                                                  source=source,
+                                                  extractor=extractor)
+                    for operation_config in configuration]
+
+        return configuration
 
 
 class ExtractorConfiguration(BaseConfiguration):
@@ -22,10 +39,9 @@ class ExtractorConfiguration(BaseConfiguration):
     IS_ENABLED_TAG = 'is_enabled'
     IMPLICIT_WAIT_TAG = 'wait'
 
-    # All wait times and delays in seconds
-    IMPLICIT_WAIT_DEFAULT = 3
+    IMPLICIT_WAIT_DEFAULT = 3  # seconds
 
-    configuration_file_cache = FileCache(maxsize=None)
+    file_cache = FileCache(maxsize=None)
 
     def __init__(self, is_enabled, implicit_wait, delay, content):
 
@@ -35,11 +51,11 @@ class ExtractorConfiguration(BaseConfiguration):
         self.content = content
 
     @classmethod
-    def from_file(cls, file_path, source, extractor):
+    def from_file(cls, file_path, extractor):
         constructor_class = (cls if cls is not ExtractorConfiguration
                              else cls._derive_class_from_file_path(file_path))
         marshalled = constructor_class._marshal_from_file(file_path)
-        return constructor_class.from_dict(marshalled, source, extractor)
+        return constructor_class.from_dict(configuration=marshalled, extractor=extractor)
 
     @classmethod
     def _derive_class_from_file_path(cls, file_path):
@@ -51,37 +67,44 @@ class ExtractorConfiguration(BaseConfiguration):
             raise ValueError(f'Invalid file path: {file_path}')
 
     @classmethod
-    @configuration_file_cache
+    @file_cache
     def _marshal_from_file(cls, file_path):
         """Marshall configuration dictionary from file, given a path"""
         with open(file_path) as stream:
             return yaml.safe_load(stream)
 
     @classmethod
-    def _derive_init_kwargs(cls, configuration, source, extractor):
+    def from_dict(cls, configuration, extractor):
+        init_kwargs = cls._derive_init_kwargs(configuration, extractor=extractor)
+        return cls(**init_kwargs)
+
+    @classmethod
+    def _derive_init_kwargs(cls, configuration, extractor, source):
         return dict(
             is_enabled=configuration.get(cls.IS_ENABLED_TAG, True),
             implicit_wait=configuration.get(cls.IMPLICIT_WAIT_TAG, cls.IMPLICIT_WAIT_DEFAULT),
             delay=DelayConfiguration.from_dict(configuration.get(DelayConfiguration.DELAY_TAG)),
             content=ContentConfiguration.from_dict(
                 configuration[ContentConfiguration.CONTENT_TAG],
-                source,
-                extractor))
-
-    @classmethod
-    def from_dict(cls, configuration, source, extractor):
-        init_kwargs = cls._derive_init_kwargs(configuration, source, extractor)
-        return cls(**init_kwargs)
+                extractor=extractor,
+                source=source))
 
 
 class SourceExtractorConfiguration(ExtractorConfiguration):
     """SourceExtractorConfiguration is currently a subset of Multi..."""
     FILE_NAME = 'source.yaml'
 
+    @classmethod
+    def _derive_init_kwargs(cls, configuration, extractor):
+        return super()._derive_init_kwargs(configuration,
+                                           extractor=extractor,
+                                           source=extractor.page_url)
+
 
 class MultiExtractorConfiguration(ExtractorConfiguration):
 
     FILE_NAME = 'multi.yaml'
+    CONTENT_ITEMS_TAG = 'items'
     EXTRACT_SOURCES_TAG = 'extract_sources'
     FRESHNESS_THRESHOLD_TAG = 'freshness_threshold'
 
@@ -96,7 +119,7 @@ class MultiExtractorConfiguration(ExtractorConfiguration):
                  content,
                  url,
                  pagination,
-                 items):
+                 content_items):
 
         super().__init__(is_enabled=is_enabled,
                          implicit_wait=implicit_wait,
@@ -107,24 +130,32 @@ class MultiExtractorConfiguration(ExtractorConfiguration):
         self.freshness_threshold = freshness_threshold
         self.url = url
         self.pagination = pagination
-        self.items = items
+        self.content_items = content_items
 
     @classmethod
-    def _derive_init_kwargs(cls, configuration, source, extractor):
-        init_kwargs = super()._derive_init_kwargs(configuration, source, extractor)
+    def from_dict(cls, configuration, extractor):
+        return super().from_dict(configuration=configuration, extractor=extractor)
+
+    @classmethod
+    def _derive_init_kwargs(cls, configuration, extractor):
+        url = URLConstructor.from_dict(configuration[URLConstructor.URL_TAG])
+
+        init_kwargs = super()._derive_init_kwargs(configuration,
+                                                  extractor=extractor,
+                                                  source=url.url_template)
 
         init_kwargs.update(
             extract_sources=configuration.get(cls.EXTRACT_SOURCES_TAG, True),
             freshness_threshold=cls._derive_freshness_threshold(configuration),
-            url=URLConstructor.from_dict(configuration[URLConstructor.URL_TAG]),
+            url=url,
             pagination=PaginationConfiguration.from_dict(
-                configuration.get(PaginationConfiguration.PAGINATION_TAG),
-                source,
-                extractor),
-            items=ItemsConfiguration.from_dict(
-                configuration[ItemsConfiguration.ITEMS_TAG],
-                source,
-                extractor))
+                configuration=PaginationConfiguration.get_dict(configuration),
+                extractor=extractor,
+                source=url.url_template),
+            content_items=cls.configure_field(configuration=configuration[cls.CONTENT_ITEMS_TAG],
+                                              field=cls.CONTENT_ITEMS_TAG,
+                                              source=url.url_template,
+                                              extractor=extractor))
 
         return init_kwargs
 
@@ -159,95 +190,74 @@ class DelayConfiguration(HumanDwellTime, BaseConfiguration):
                 type='unsupported_keys_in_delay_configuration',
                 unsupported=unsupported))
 
-    def as_human_dwell_time(self):
-        return HumanDwellTime(**self)
 
-
-class FieldConfigurable:
-
-    @classmethod
-    def configure_field(cls, configuration, field, source, extractor):
-        # TODO: utilize contextvars for field/source (py3.7)
-        if isinstance(configuration, dict):
-            return ExtractionOperation.from_dict(configuration=configuration,
-                                                 field=field,
-                                                 source=source,
-                                                 extractor=extractor)
-
-        if isinstance(configuration, list):
-            return [ExtractionOperation.from_dict(configuration=operation_config,
-                                                  field=field,
-                                                  source=source,
-                                                  extractor=extractor)
-                    for operation_config in configuration]
-
-        return configuration
-
-
-class ItemsConfiguration(FieldConfigurable, BaseConfiguration):
-
-    ITEMS_TAG = 'items'
-
-    def __init__(self, items):
-        self.items = items
-
-    @classmethod
-    def from_dict(cls, configuration, source, extractor):
-        items = cls.configure_field(configuration=configuration,
-                                    field=cls.ITEMS_TAG,
-                                    source=source,
-                                    extractor=extractor)
-        return cls(items=items)
-
-
-class ContentConfiguration(FieldConfigurable, BaseConfiguration):
+class ContentConfiguration(BaseConfiguration):
 
     CONTENT_TAG = 'content'
 
     @classmethod
-    def from_dict(cls, configuration, source, extractor):
+    def from_dict(cls, configuration, extractor, source):
         inst = cls()
         for field, value in configuration.items():
-            inst[field] = cls.configure_field(value, field, source, extractor)
+            inst[field] = cls.configure_field(configuration=value,
+                                              field=field,
+                                              source=source,
+                                              extractor=extractor)
         return inst
 
 
-class PaginationConfiguration(FieldConfigurable, BaseConfiguration):
+class PaginationConfiguration(BaseConfiguration):
 
     PAGINATION_TAG = 'pagination'
     PAGES_TAG = 'pages'
     PAGE_SIZE_TAG = 'page_size'
-    NEXT_PAGE_TAG = 'next_page'  # TODO: replace with click/url below
     NEXT_PAGE_CLICK_TAG = 'next_page_click'
     NEXT_PAGE_URL_TAG = 'next_page_url'
 
+    PAGES_MINIMUM_DEFAULT = 1
     PAGES_MAXIMUM_DEFAULT = 10
 
-    NextPageMethod = FlexEnum('NextPageMethod', 'CLICK URL')
+    NextPageVia = FlexEnum('NextPageVia', 'CLICK URL')
 
-    def __init__(self, pages=1, page_size=None, next_page=None, next_page_method=None):
+    def __init__(self, pages=PAGES_MINIMUM_DEFAULT, page_size=None, next_page=None, next_page_method=None):
         self.pages = pages
         self.page_size = page_size
         self.next_page = next_page
         self.next_page_method = next_page_method
 
     @classmethod
-    def from_dict(cls, configuration, source, extractor):
+    def from_dict(cls, configuration, extractor, source):
         if not configuration:
             return cls()
 
         next_page_click = configuration.get(cls.NEXT_PAGE_CLICK_TAG)
         next_page_url = configuration.get(cls.NEXT_PAGE_URL_TAG)
         next_page_configuration = xor_constrain(next_page_click, next_page_url)
-        next_page_method = cls.NextPageMethod.CLICK if next_page_click else cls.NextPageMethod.URL
+        next_page_method = cls.NextPageVia.CLICK if next_page_click else cls.NextPageVia.URL
         next_page_field = cls.NEXT_PAGE_CLICK_TAG if next_page_click else cls.NEXT_PAGE_URL_TAG
 
         next_page = cls.configure_field(configuration=next_page_configuration,
                                         field=next_page_field,
-                                        source=source,
-                                        extractor=extractor)
+                                        extractor=extractor,
+                                        source=source)
 
         return cls(pages=configuration.get(cls.PAGES_TAG, cls.PAGES_MAXIMUM_DEFAULT),
                    page_size=configuration[cls.PAGE_SIZE_TAG],
                    next_page=next_page,
                    next_page_method=next_page_method)
+
+    @classmethod
+    def get_dict(cls, configuration):
+        return configuration.get(cls.PAGINATION_TAG)
+
+    @property
+    def next_page_tag(self):
+        return self.NEXT_PAGE_CLICK_TAG if self.via_click else self.NEXT_PAGE_URL_TAG
+
+    @property
+    def via_click(self):
+        return self.next_page_method is self.NextPageVia.CLICK
+
+    @property
+    def via_url(self):
+        return self.next_page_method is self.NextPageVia.URL
