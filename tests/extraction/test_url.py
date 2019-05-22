5e109a705b16822c8cd3cd59ce1677fc0d01b447
Eli Chan <eli.chan@intertwine.io>
2019-05-22 18:39:36 -0500

    Fix URL constructor bug affecting user agent extraction

--- tests/extraction/test_url.py
+++ tests/extraction/test_url.py
@@ -9,6 +9,11 @@ from contextualize.utils.tools import is_child_class
 
 
 URL_CONFIGURATION_A = {
+    'url_template': ('https://developers.whatismybrowser.com/'
+                     'useragents/explore/software_name/{browser}/1')
+}
+
+URL_CONFIGURATION_B = {
     'url_template': 'https://academic.oup.com/journals/search-results?page=1&qb={{query}}',
     'query': {
         'series': 'TOPIC',
@@ -19,7 +24,7 @@ URL_CONFIGURATION_A = {
     'qualifier_clause': '%22FullText{index}%22:%22{term}%22',
 }
 
-URL_CONFIGURATION_B = {
+URL_CONFIGURATION_C = {
     'url_template': 'https://www.ncbi.nlm.nih.gov/pmc/?term={query}',
     'query': {
         'series': 'TOPIC',
@@ -38,80 +43,86 @@ URL_CONFIGURATION_B = {
 @pytest.mark.parametrize(
     'idx, configuration, search_data, check', [
     (
-        0,  # URL construction involving index and TOPIC URL clause
+        0,  # URL construction involving just a search term
         URL_CONFIGURATION_A,
+        OrderedDict(browser='chrome'),
+        'https://developers.whatismybrowser.com/useragents/explore/software_name/chrome/1'
+     ),
+    (
+        1,  # URL construction involving index and TOPIC URL clause
+        URL_CONFIGURATION_B,
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         'https://academic.oup.com/journals/search-results?page=1&qb='
         '{%22Keywords1%22:%22Homelessness%22,%22FullText2%22:%22Texas%22}'
      ),
     (
-        1,  # URL construction involving TOPIC and TERM URL clauses
-        URL_CONFIGURATION_B,
+        2,  # URL construction involving TOPIC and TERM URL clauses
+        URL_CONFIGURATION_C,
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         'https://www.ncbi.nlm.nih.gov/pmc/?term='
         '(Homelessness%5BAbstract%5D)+AND+(Texas%5BAbstract%5D+OR+TX%5BAbstract%5D)'
      ),
     (
-        2,  # Hardcoded URL
+        3,  # Hardcoded URL
         'http://www.example.com',
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         'http://www.example.com'
      ),
     (
-        3,  # Empty configuration dictionary
+        4,  # Empty configuration dictionary
         {},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         TypeError
      ),
     (
-        4,  # Missing URL_TEMPLATE_TAG key
-        {k: v for k, v in URL_CONFIGURATION_A.items() if k != URLConstructor.URL_TEMPLATE_TAG},
+        5,  # Missing URL_TEMPLATE_TAG key
+        {k: v for k, v in URL_CONFIGURATION_B.items() if k != URLConstructor.URL_TEMPLATE_TAG},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         TypeError
      ),
     (
-        5,  # Missing 'query' key
-        {k: v for k, v in URL_CONFIGURATION_A.items() if k != 'query'},
+        6,  # Missing 'query' key
+        {k: v for k, v in URL_CONFIGURATION_B.items() if k != 'query'},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         ValueError
      ),
     (
-        6,  # Missing 'primary_clause' key
-        {k: v for k, v in URL_CONFIGURATION_A.items() if k != 'primary_clause'},
+        7,  # Missing 'primary_clause' key
+        {k: v for k, v in URL_CONFIGURATION_B.items() if k != 'primary_clause'},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         ValueError
      ),
     (
-        7,  # Missing 'qualifier_clause' key
-        {k: v for k, v in URL_CONFIGURATION_A.items() if k != 'qualifier_clause'},
+        8,  # Missing 'qualifier_clause' key
+        {k: v for k, v in URL_CONFIGURATION_B.items() if k != 'qualifier_clause'},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         ValueError
      ),
     (
-        8,  # Missing 'series' key from 'query' URL clause
+        9,  # Missing 'series' key from 'query' URL clause
         {k: v if k != 'query' else {k2: v2 for k2, v2 in v.items() if k2 != 'series'}
-         for k, v in URL_CONFIGURATION_A.items()},
+         for k, v in URL_CONFIGURATION_B.items()},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         KeyError
      ),
     (
-        9,  # Missing 'templates' key from 'query' URL clause
+        10,  # Missing 'templates' key from 'query' URL clause
         {k: v if k != 'query' else {k2: v2 for k2, v2 in v.items() if k2 != 'templates'}
-         for k, v in URL_CONFIGURATION_A.items()},
+         for k, v in URL_CONFIGURATION_B.items()},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         KeyError
      ),
     (
-        10,  # Missing 'delimiter' key from 'query' URL clause
+        11,  # Missing 'delimiter' key from 'query' URL clause
         {k: v if k != 'query' else {k2: v2 for k2, v2 in v.items() if k2 != 'delimiter'}
-         for k, v in URL_CONFIGURATION_A.items()},
+         for k, v in URL_CONFIGURATION_B.items()},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         KeyError
      ),
     (
-        11,  # 'TOPIC' misspelled as 'TROPIC' in 'query' URL clause
+        12,  # 'TOPIC' misspelled as 'TROPIC' in 'query' URL clause
         {k: v if k != 'query' else {k2: v2 if k2 != 'series' else 'TROPIC' for k2, v2 in v.items()}
-         for k, v in URL_CONFIGURATION_A.items()},
+         for k, v in URL_CONFIGURATION_B.items()},
         OrderedDict([('problem', 'Homelessness'), ('org', None), ('geo', ['Texas', 'TX'])]),
         KeyError
      ),
